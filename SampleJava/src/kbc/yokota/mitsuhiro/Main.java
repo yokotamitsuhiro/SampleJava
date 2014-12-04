0eb6897e934a64b13a08aaef8ad92c7592c7fd13
yokotamitsuhiro <kbc11a15@stu.kawahara.ac.jp>
2014-12-04 10:12:59 +0900

    FirstCommit

---
+++ SampleJava/src/kbc/yokota/mitsuhiro/Main.java
@@ -0,0 +1,13 @@
+package kbc.yokota.mitsuhiro;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello EGit !");
+	}
+
+}
